5125efddb05f6dcc45406e54096f45660e6d26a6
Matheus Canovas Almeida Santos <msantos@agecomnet.com.br>
2025-05-15 21:45:13 -0300

    implementing course business and service interface

---
+++ src/main/java/com/github/mattcanovas/business/CourseBusiness.java
@@ -0,0 +1,23 @@
+package com.github.mattcanovas.business;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.mattcanovas.services.ICourseService;
+
+public class CourseBusiness {
+
+    private ICourseService service;
+
+    public CourseBusiness(ICourseService service) {
+        this.service = service;
+    }
+
+    public List<String> retrieveCoursesRelatedToSpring(String student) {
+        return service.retrieveCourses(student)
+            .stream()
+            .filter((course) -> course.contains("Spring"))
+            .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/com/github/mattcanovas/services/ICourseService.java
@@ -0,0 +1,9 @@
+package com.github.mattcanovas.services;
+
+import java.util.List;
+
+public interface ICourseService {
+    
+    public List<String> retrieveCourses(String student);
+
+}
